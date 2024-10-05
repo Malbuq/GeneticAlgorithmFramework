9513ee7907bbf135462f77ae35d580c9663da9ea
Michael Albuquerque <michael.albqq@gmail.com>
2024-10-05 08:51:11 -0300

    feat: adding finishing the interface for the evaluators

--- Framework/FitnessEvaluatorStrategies/FitnessEvaluatorStrategy.java
+++ Framework/FitnessEvaluatorStrategies/FitnessEvaluatorStrategy.java
@@ -1,5 +1,9 @@
-public interface FitnessEvaluatorStrategy {
-    public float calculatePopulationFitness();
-    public float calculateChromosomeFitness();
-    public float calculateGeneFitness();
+import Framework.ChromosomeInterface;
+import Framework.GeneInterface;
+import Framework.PopulationInterface;
+
+public interface FitnessEvaluatorStrategy<T extends Number> {
+    public float calculatePopulationFitness(PopulationInterface<T> population);
+    public float calculateChromosomeFitness(ChromosomeInterface<T> chromosome);
+    public float calculateGeneFitness(GeneInterface<T> gene);
 }
