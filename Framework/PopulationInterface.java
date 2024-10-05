2526755513e60813745e0ea4b55944978a200312
Michael Albuquerque <michael.albqq@gmail.com>
2024-10-05 09:44:11 -0300

    fix: changing folder structure again

--- Framework/PopulationInterface.java
+++
@@ -1,9 +0,0 @@
-package PopulationComponents;
-
-public interface PopulationInterface<T extends Number> {
-    public int getSize();
-    public void setSize(int size);
-
-    public void addChromosome(ChromosomeInterface<T> chromosome);
-    public ChromosomeInterface<T> getChromosomeAt(int index);
-}
