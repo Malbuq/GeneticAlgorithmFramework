02c3720ee38f0f345df4ad8a12c3666cab7cf0b5
Michael Albuquerque <michael.albqq@gmail.com>
2024-10-06 10:07:13 -0300

    feat: adding one point crossover strategy

--- Framework/Strategies/ChromosomeCrossoverStrategy.java
+++ Framework/Strategies/ChromosomeCrossoverStrategy.java
@@ -1,7 +1,10 @@
 package Framework.Strategies;
 
+import Framework.PopulationComponents.ChromosomeInterface;
 import Framework.PopulationComponents.PopulationInterface;
 
 public interface ChromosomeCrossoverStrategy {
-    public PopulationInterface crossover(PopulationInterface population, float crossoverProbability);
+    public PopulationInterface crossoverPopulation(PopulationInterface population, float crossoverProbability);
+    public ChromosomeInterface crossoverChromosomes(ChromosomeInterface father, ChromosomeInterface mother);
+
 }
---
+++ Framework/Test/OnePointCrossoverStrategy.java
@@ -0,0 +1,81 @@
+package Framework.Test;
+
+import java.util.Random;
+
+import Framework.PopulationComponents.ChromosomeInterface;
+import Framework.PopulationComponents.PopulationInterface;
+import Framework.Strategies.ChromosomeCrossoverStrategy;
+
+public class OnePointCrossoverStrategy implements ChromosomeCrossoverStrategy {
+
+    @Override
+    public PopulationInterface crossoverPopulation(PopulationInterface currentPopulation, float crossoverProbability) {
+        int populationSize = currentPopulation.getSize();
+        Population newPopulation = new Population(populationSize, currentPopulation.getleftDomainBoundarie(),
+                currentPopulation.getRightDomainBoundarie());
+        int randomChomosomeIndex;
+        Random random = new Random();
+        int newPopulationPointer = 0;
+
+        boolean dontCrossover;
+        boolean isPopulationOdd = populationSize % 2 != 0;
+
+        if (isPopulationOdd) {
+            randomChomosomeIndex = random.nextInt(populationSize);
+            ChromosomeInterface randomChromosome = currentPopulation.getChromosomeAt(randomChomosomeIndex);
+            newPopulation.setChromosomeAt(newPopulationPointer, randomChromosome);
+            newPopulationPointer++;
+        }
+
+        while (newPopulationPointer < populationSize) {
+            randomChomosomeIndex = random.nextInt(populationSize);
+            ChromosomeInterface chromosomeFather = currentPopulation.getChromosomeAt(randomChomosomeIndex);
+
+            randomChomosomeIndex = random.nextInt(populationSize);
+            ChromosomeInterface chromosomeMother = currentPopulation.getChromosomeAt(randomChomosomeIndex);
+
+            double randomProbability = Math.random();
+
+            dontCrossover = randomProbability > crossoverProbability;
+
+            if (dontCrossover) {
+                newPopulation.setChromosomeAt(newPopulationPointer, chromosomeFather);
+                newPopulationPointer++;
+
+                newPopulation.setChromosomeAt(newPopulationPointer, chromosomeMother);
+                newPopulationPointer++;
+                continue;
+            }
+
+            
+            ChromosomeInterface firstChild = this.crossoverChromosomes(chromosomeFather, chromosomeMother);
+            ChromosomeInterface secondChild = this.crossoverChromosomes(chromosomeMother, chromosomeFather);
+            
+            newPopulation.setChromosomeAt(newPopulationPointer, firstChild);
+            newPopulationPointer++;
+            
+            newPopulation.setChromosomeAt(newPopulationPointer, secondChild);
+            newPopulationPointer++;
+        }
+        
+        return newPopulation;
+    }
+    
+    public ChromosomeInterface crossoverChromosomes(ChromosomeInterface chromosomeLeft, ChromosomeInterface chromosomeRight) {
+        Random random = new Random();
+        int chromosomeLength = chromosomeLeft.getLength();
+        ChromosomeInterface child = new Chromosome(chromosomeLength);
+        int crossoverPoint = random.nextInt(chromosomeLength);
+
+        for(int geneIndex = 0; geneIndex < chromosomeLength; geneIndex++) {
+            if(geneIndex < crossoverPoint) {
+                child.setGeneAt(geneIndex, chromosomeLeft.getGeneAt(geneIndex));
+                continue;
+            }
+            child.setGeneAt(geneIndex, chromosomeRight.getGeneAt(geneIndex));
+        }
+
+        return child;
+    }
+
+}
